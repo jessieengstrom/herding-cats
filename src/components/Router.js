5ad77a34b46f87dfb787b5ed8ab2eafc9bfc3cc5
Jessie Engstrom <jessielengstrom@gmail.com>
2018-05-22 19:52:48 -0700

    addeda a react router

---
+++ src/components/Router.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from './App';
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' component={App}>
+        </Switch>
+                
+)
